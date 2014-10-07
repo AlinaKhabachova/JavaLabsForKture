ab6f9dfc502e08e0043275532c8dddeaa3a4ca7c
AlinaKhabachova <alina.khabachyova@gmail.com>
2014-10-07 14:04:15 +0300

    lab1var1

---
+++ src/com/alina/labs/lab1/var1/Var1.java
@@ -0,0 +1,25 @@
+package com.alina.labs.lab1.var1;
+
+public class Var1 
+{
+	
+	public static void main(String[] args) 
+	{
+		int first =1;
+		int last = 100;
+		
+		nextNumber: for(int i=first; i<=last; i++)
+		{
+			for(int j=2; j<i; j++)
+			{
+				if(i%j==0)	continue nextNumber;
+				else
+				{
+					System.out.print(i+", "); 	
+					continue nextNumber;
+				}
+			}	
+		}
+	}
+}
+
